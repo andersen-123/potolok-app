b2d4cdbcf359043b3e418bd9318e04eb8883e504
andersen-123 <121757418+andersen-123@users.noreply.github.com>
2025-12-17 23:29:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Импортируем Bootstrap правильно для React
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
